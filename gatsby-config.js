8021b60a7af5c5a7661e430aee94ceab94a0f44e
Chris <u4y0b@ugrad.cs.ubc.ca>
2020-09-02 18:23:35 -0700

    add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,20 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  pathPrefix: "/spectogram",
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Spectogram`,
+        short_name: `Spec`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: "src/images/wave.svg",
+      },
+    },
+    "gatsby-plugin-offline",
+  ],
 }
