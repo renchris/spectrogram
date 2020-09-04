648854b195ee001f1f515f8b9ab4142d9ff44f72
Chris <u4y0b@ugrad.cs.ubc.ca>
2020-09-03 23:12:04 -0700

    complete Spectogram with header and styling

--- README.md
+++ README.md
@@ -5,23 +5,15 @@
   </a>
 </p>
 <h1 align="center">
-  Gatsby's hello-world starter
+  Spectogram
 </h1>
-
-Kick off your project with this hello-world boilerplate. This starter ships with the main Gatsby configuration files you might need to get up and running blazing fast with the blazing fast app generator for React.
-
-_Have another more specific idea? You may want to check out our vibrant collection of [official and community-created starters](https://www.gatsbyjs.com/docs/gatsby-starters/)._
-
-## 🚀 Quick start
-
-# Spectogram
 
 A real-time audio processing module that records audio from the microphone and visualizes the loudness of the audio signal.
 
 Vertical height represents the frequency from low to high tones in Hz.
 Colour represents the loudness in Db.
 
-## Usage
+## 🚀 Usage
 
 Link: https://renchris.github.io/spectrogram/
 
@@ -92,19 +84,5 @@ A quick look at the top-level files and directories you'll see in a Gatsby proje
 11. **`package.json`**: A manifest file for Node.js projects, which includes things like metadata (the project’s name, author, etc). This manifest is how npm knows which packages to install for your project.
 
 12. **`README.md`**: A text file containing useful reference information about your project.
-
-## 🎓 Learning Gatsby
-
-Looking for more guidance? Full documentation for Gatsby lives [on the website](https://www.gatsbyjs.com/). Here are some places to start:
-
-- **For most developers, we recommend starting with our [in-depth tutorial for creating a site with Gatsby](https://www.gatsbyjs.com/tutorial/).** It starts with zero assumptions about your level of ability and walks through every step of the process.
-
-- **To dive straight into code samples, head [to our documentation](https://www.gatsbyjs.com/docs/).** In particular, check out the _Guides_, _API Reference_, and _Advanced Tutorials_ sections in the sidebar.
-
-## 💫 Deploy
-
-[![Deploy to Netlify](https://www.netlify.com/img/deploy/button.svg)](https://app.netlify.com/start/deploy?repository=https://github.com/gatsbyjs/gatsby-starter-hello-world)
-
-[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/import/project?template=https://github.com/gatsbyjs/gatsby-starter-hello-world)
 
 <!-- AUTO-GENERATED-CONTENT:END -->
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,113 @@
 import React from "react"
 
+import layoutStyles from "./index.css"
+
+import image from "../images/wave.svg"
+
+var canvasActivated = false
+
+function setupCanvas() {
+  console.log("That's cool")
+
+  if (canvasActivated) {
+    return
+  }
+  canvasActivated = true
+
+  document.getElementById("clickHeader").hidden = true
+
+  // //cavnas
+  var canvas = document.getElementById("specCanvas")
+  console.log(canvas)
+  // //2D context of canvas
+  var contextOfCanvas = canvas.getContext("2d")
+
+  const canvasWidth = (canvas.width = window.innerWidth)
+  const canvasHeight = (canvas.height = window.innerHeight)
+
+  console.log(canvasWidth)
+  console.log(canvasHeight)
+
+  //connect our stream to our analyzer, and ask our anaylzer for specific types of data
+  //ie frequency spectrum at our current point in time
+  const audioContext = new AudioContext()
+  const analyser = audioContext.createAnalyser()
+
+  //how detailed our data is going to be
+  analyser.fftSize = 1024
+
+  // microphone
+  navigator.mediaDevices.getUserMedia({ audio: true }).then(process)
+
+  //takes our audio stream
+  function process(audioStream) {
+    console.log(audioStream)
+    //wraps stream in a source object allows us to connect our stream to our analyzer
+    const audioSource = audioContext.createMediaStreamSource(audioStream)
+    audioSource.connect(analyser)
+    //array of audio data with  has length of audioContext.fftSize
+    const audioData = new Uint8Array(analyser.frequencyBinCount)
+    const dataLength = audioData.length
+
+    const lineHeight = canvasHeight / dataLength
+    const xPosition = canvasWidth - 1
+
+    contextOfCanvas.fillStyle = `hsl(280, 100%, 10%)`
+    contextOfCanvas.fillRect(0, 0, canvasWidth, canvasHeight)
+
+    loop()
+
+    function loop() {
+      //call again on next frame
+      window.requestAnimationFrame(loop)
+      //take data and move it one pixel to the left
+      let imageData = contextOfCanvas.getImageData(
+        1,
+        0,
+        canvasWidth - 1,
+        canvasHeight
+      )
+      contextOfCanvas.fillRect(0, 0, canvasWidth, canvasHeight)
+      contextOfCanvas.putImageData(imageData, 0, 0)
+      //analyser will populate and transform data array
+      analyser.getByteFrequencyData(audioData)
+      console.log(dataLength)
+      for (let i = 0; i < dataLength; i++) {
+        let ratio = audioData[i] / 255
+        let hue = (ratio * 120 + 280) % 360
+        let saturation = "100%"
+        let lightness = 10 + 70 * ratio + "%"
+        contextOfCanvas.beginPath()
+        contextOfCanvas.strokeStyle = `hsl(${hue}, ${saturation}, ${lightness})`
+        contextOfCanvas.lineWidth = 15
+        contextOfCanvas.moveTo(xPosition, canvasHeight - i * lineHeight)
+        contextOfCanvas.lineTo(
+          xPosition,
+          canvasHeight - (i * lineHeight + lineHeight)
+        )
+        contextOfCanvas.stroke()
+      }
+    }
+  }
+}
+
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <div className="image-div">
+        <a href="https://github.com/renchris/spectrogram" target="_blank">
+          <img alt="Spectrogram" src={image} />
+        </a>
+        <h1 className="image-title">Specotgram</h1>
+      </div>
+      <h1 id="clickHeader" onClick={setupCanvas}>
+        Click to Start
+      </h1>
+      <canvas
+        onClick={setupCanvas}
+        id="specCanvas"
+        className={layoutStyles.canvas}
+      ></canvas>
+    </div>
+  )
 }
